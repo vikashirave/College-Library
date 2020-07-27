729a6990e9011f6e11465859a87b9a480c3563ab
Vikas Hirave <45948809+vikashirave@users.noreply.github.com>
2020-07-27 23:37:46 +0530

    commit

---
+++ app/app.js
@@ -0,0 +1,27 @@
+console.log("This is app.js");
+
+//Constructor
+function Book(name, author, type){
+    this.name = name;
+    this.author = author;
+    this.type = type;
+};
+ 
+//Display Constructor
+function Display(){
+
+}
+
+//Add methods to Display prototype
+
+
+
+
+
+//Add submit event listener to libraryForm
+let libraryForm = document.getElementById('libraryForm');
+libraryForm.addEventListener('submit', libraryFormSubmit);
+
+function libraryFormSubmit(){
+    console.log('You have submitted library from !'); 
+}
